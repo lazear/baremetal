cc6c86d3ea8347921b170969eb2e5541320cefde
Michael Lazear <lazear@scripps.edu>
2016-08-25 00:12:24 -0700

    Buffered IDE r/w capabilities added

--- kernel/ide.c
+++ kernel/ide.c
@@ -1,6 +1,6 @@
 /*
 ide.c
-===============================================================================
+================================================================================
 MIT License
 Copyright (c) 2007-2016 Michael Lazear
 
@@ -21,8 +21,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-===============================================================================
-Disk buffer cache/LBA read/write support
+================================================================================
+Disk buffer cache/LBA read/write support ala XV6
 */
 
 #include <types.h>
@@ -30,9 +30,11 @@ Disk buffer cache/LBA read/write support
 #include <mutex.h>
 #include <traps.h>
 #include <ide.h>
+#include <assert.h>
 
 void sleep(void* channel, mutex* m) {}
 void wake(void* channel) {} 
+
 
 mutex idelock = {.lock = 0};		// spinlock for disk access
 buffer* idequeue;					// cache of buffer access
@@ -47,7 +49,7 @@ static int ide_wait(int check) {
 	char r;
 
 	// Wait while drive is busy. Once just ready is set, exit the loop
-	while (((r = (char)inb(IDE_IO | IDE_CMD)) & (IDE_BSY | IDE_RDY)) != IDE_RDY );
+	while (((r = (char)inb(IDE_IO | IDE_CMD)) & (IDE_BSY | IDE_RDY)) != IDE_RDY);
 
 	// Check for errors
 	if (check && (r & (IDE_DF | IDE_ERR)) != 0)
@@ -69,6 +71,7 @@ static void ide_op(buffer* b) {
 
 	int sector_per_block = BLOCK_SIZE / SECTOR_SIZE;	// 1
 	int sector = b->block * sector_per_block;
+
 	ide_wait(0);
 	outb(IDE_ALT, 0);	// Generate interrupt
 	outb(IDE_IO | IDE_SECN, sector_per_block);	// # of sectors
@@ -80,7 +83,7 @@ static void ide_op(buffer* b) {
 
 	if (b->flags & B_DIRTY) {						// Need to write
 		outb(IDE_IO | IDE_CMD, IDE_CMD_WRITE);
-		outsl(IDE_IO, b->data, BLOCK_SIZE / 4);		// Write in 4 byte chunks (double word)
+		outsl(IDE_IO, b->data, BLOCK_SIZE / 4);		// Write in 4 byte chunks
 	} else {										// Read only
 		outb(IDE_IO | IDE_CMD, IDE_CMD_READ);
 	}
@@ -101,6 +104,7 @@ void ide_handler() {
 
 	//if dirty is not set, and no errors, read data
 	int stat = ide_wait(1);
+	assert(stat >= 0);
 	if (!(b->flags & B_DIRTY) && stat >= 0)
 		insl(0x1f0, b->data, BLOCK_SIZE/4);
 
@@ -145,21 +149,19 @@ int ide_rw(buffer* b) {
 	if (b->dev != 1)
 		return -2;
 
-	acquire(&idelock);
 	buffer** pp;
 	for(pp = &idequeue; *pp; pp=&(*pp)->next) 
 		;	// Cycle through all valid next's.
 	*pp = b;	// b is now appended.
-	printf("%x, %x\n", pp, &idequeue);
 
+	assert(idequeue == b);
 	if (idequeue == b)
 		ide_op(b);	// If this is only item in queue, start operation.
 
-
-	while( b->flags & (B_VALID | B_DIRTY) != B_VALID)
+	//assert( b->flags & (B_VALID | B_DIRTY) != B_VALID);
+	//while( b->flags & (B_VALID | B_DIRTY) != B_VALID);
 		sleep(b, &idelock);	// Sleep until VALID is set and DIRTY is clear
 
-	release(&idelock);
 	return 0;
 }
 
@@ -178,10 +180,95 @@ void ide_test() {
 
 	int ticks = get_ticks();
 	while(ticks + 20 > get_ticks());
+}
 
+
+struct {
+	mutex lock;
+	buffer* list;
+} cache;
+
+void buffer_init() {
+	cache.list = malloc(sizeof(buffer) * MAX_OP_BLOCKS);
+	int i = 0;
+	buffer* b;
+	for (b = cache.list; b < (cache.list + MAX_OP_BLOCKS - 1); b++) {
+		b->next = b+1;
+		b->dev = -1;
+	}
+	//b++;
+	b->next = NULL;
+	b->dev = -1;
+}
+
+void buffer_dump(buffer *b) {
+	assert(b);
 	for (int i = 0; i < BLOCK_SIZE; i++)
 		printf("%x", b->data[i]);
-	printf("\nBlock2\n");
-	for (int i = 0; i < BLOCK_SIZE; i++)
-		printf("%x", c->data[i]);
+}
+
+/* For debugging purposes */
+void buffer_traverse() {
+	buffer* b;
+	int i = 0;
+	for (b = cache.list; b; b = b->next) {
+		printf("%d this %x that %x\n", i++, b, b->next);
+	}
+}
+
+buffer* buffer_get(uint32_t dev, uint32_t block) {
+	buffer* b;
+	acquire(&cache.lock);
+loop:
+	for (b = cache.list; b; b = b->next) {
+		if (b->dev == dev && b->block == block) {
+			if (!(b->flags & B_BUSY)) {		// Is buffer free?
+				b->flags |= B_BUSY;			// Mark buffer as in-use
+				release(&cache.lock);
+				return b;
+			}
+			sleep(b, &cache.lock);			// Wait until that block is free
+			goto loop;					// Without MT, this freezes
+		}
+	}
+
+	/* Block not in cache. See if we can find a block that is not busy,
+	and not dirty, and then return it. */
+	for (b = cache.list; b; b = b->next) {
+		if ((b->flags & B_DIRTY) == 0 && (b->flags & B_BUSY) == 0) {
+			b->dev = dev;
+			b->block = block;
+			b->flags = B_BUSY;
+			release(&cache.lock);
+			return b;
+		}
+	}
+
+	/* Worst case scenario - no free blocks. So we malloc a new block, and 
+	add it to the end of the list */
+
+	buffer** bp;
+	for (bp = &cache.list; *bp; bp = &(*bp)->next)
+		; 
+	*bp = malloc(sizeof(buffer));
+	(*bp)->dev = dev;
+	(*bp)->block = block;
+	(*bp)->flags = B_BUSY;
+
+	release(&cache.lock);
+	return *bp;
+}
+
+buffer* buffer_read(uint32_t dev, uint32_t block) {
+	buffer* b = buffer_get(dev, block);
+	int i = -1;
+	if ( !(b->flags & B_VALID)) 	// Block not read yet
+		i = ide_rw(b);
+	return b;
+}
+
+void buffer_write(buffer* b) {
+	assert((b->flags & B_BUSY) == 0);
+	b->flags |= B_DIRTY; 		// Set write flag
+	ide_rw(b);
 }
--- kernel/include/assert.h
+++ kernel/include/assert.h
@@ -1,6 +1,6 @@
 #ifndef __assert__
 #define __assert__
 
-#define assert(e) ((e) ? (void) 0: panic(#e, __FILE__, __LINE__))
+#define assert(e) ((e) ? (void) 0: panic(#e))
 
 #endif
