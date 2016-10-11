a3921c8c7de318e6179cfb8972f59a392a438a7b
Michael Lazear <lazear@scripps.edu>
2016-10-11 12:39:53 -0700

    More PMM work

--- pmm_test.c
+++ pmm_test.c
@@ -25,24 +25,26 @@ struct pages {
 	struct block blocks[0x400] __attribute__((aligned(0x1000)));
 } pp;
 
+static struct mm_info {
+	struct pages* p;
+	uint32_t last_alloc;
+	uint32_t last_free;
+	uint32_t used_pages;
+};
 
-
-int find_and_allocate(struct pages* p) {
-	for (int i = 0; i < 1024; i++) {
-		/* Do 4 byte comparisons */
-		uint32_t* bitmap = p->blocks[i].bitmap;
+/* Find the first free page and return it's address*/
+uint32_t find_and_allocate(struct pages* p) {
+	for (int block_index = 0; block_index < 1024; block_index++) {
+		uint32_t* bitmap = p->blocks[block_index].bitmap;
 		for (int q = 0; q < 32; q++)
-			
 			if (bitmap[q] != ~0){	/* bitmap with a free bit */
-				uint32_t t = bitmap[q] ^ ~0x0;	/* Free bits */
-
+				uint32_t free_bits = bitmap[q] ^ ~0x0;	/* Free bits */
 				/* Test each bit */
 				for (int j = 0; j < 32; j++)
-					if (TEST(t, j)) {
-						int f = j + (q * 32) + (i * 1024);
-						SET(p->blocks[i].bitmap[q], j);
-						printf("bit free %5d address %8x : free bits: %8x used bits: %8x\n", f, (f*0x1000), t, t ^ 0xFFFFFFFF);
-						return f * 0x1000;
+					if (TEST(free_bits, j)) {
+						SET(bitmap, j);
+						int page = j + (q * 32) + (block_index * 1024);
+						return page * 0x1000;
 					}
 			}
 	}
@@ -57,23 +59,32 @@ int find_and_mark(struct pages* p, uint32_t address, int val) {
 
 	uint32_t* bitmap = p->blocks[i].bitmap;
 
-	if (val) 
+	if (val == 1) 
 		SET(bitmap[q], j);
-	else
+	if (val == 0)
 		CLEAR(bitmap[q], j);
+
+	return TEST(bitmap[q], j);
 }
 
-int find_and_free(struct pages* p, uint32_t address) {
-	int i = (address / (0x00400000));				/* Each block represents 4 mb, which block index into page structure */
-	int q = (address / 0x20000) % 32;				/* What is the bitmap array index? */
-	int j = (address - (q*0x20000)) / 0x1000 % 32;	/* what bitmap bit are we? */
-
-	uint32_t* bitmap = p->blocks[i].bitmap;
-	CLEAR(bitmap[q], j);
+/* Allocates the next available physical address */
+uint32_t mm_alloc() {
+	return find_and_allocate(&pp);
 }
 
+/* De-allocates a specific physical address */
+uint32_t mm_free(uint32_t address) {
+	return find_and_mark(&pp, address, 0);
+}
 
-
+/* Request allocation of a specific physical address
+Returns 1 on success, -1 on failure */
+int mm_request(uint32_t address) {
+	int used = find_and_mark(&pp, address, -1);
+	if (!used)	
+		return find_and_mark(&pp, address, 1);
+	return -1;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -83,12 +94,10 @@ int main(int argc, char* argv[]) {
 		for(int i = 0; i < 32; i++)
 			pp.blocks[q].bitmap[i] = ~0;
 
-	int x = 1023;
-	pp.blocks[x].bitmap[2] = 0x0EFB;
-	while(pp.blocks[x].bitmap[2] != ~0) {
-		int f = find_and_allocate(&pp);	
-		find_and_free(&pp, f);
-	}
+	printf("%+d\n", mm_request(0));
+	mm_free(0);
+	printf("%+d\n", mm_request(0));
+
 
 
 
